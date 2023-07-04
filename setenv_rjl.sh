cfffc7171ee63eb82fc27ad165df7959ab04ac9e
Randy LeVeque <rjl@uw.edu>
2023-07-04 14:52:05 -0700

    petscMPIoptions used, and setenv file for rjl laptop

---
+++ setenv_rjl.sh
@@ -0,0 +1,11 @@
+
+export BoussDev=/Users/rjl/git/BoussDev
+export CLAW=$BoussDev/clawpack
+export PYTHONPATH=$CLAW
+
+export PETSC_DIR=/Users/rjl/git/Clones/petsc
+export PETSC_ARCH=$PETSC_DIR/arch-darwin-c-opt
+export PETSC_OPTIONS="-options_file $BoussDev/rjl_examples/petscMPIoptions"
+export OMP_NUM_THREADS=6
+
+PS1='[\W] bouss $ '
