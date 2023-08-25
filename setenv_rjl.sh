c525fd0eaa5577252bc60151b9686ef3af93efd3
Randy LeVeque <rjl@uw.edu>
2023-08-25 10:57:02 -0700

    point to proper petscMPIoptions file

--- setenv_rjl.sh
+++ setenv_rjl.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=$CLAW
 
 export PETSC_DIR=/Users/rjl/git/Clones/petsc
 export PETSC_ARCH=$PETSC_DIR/arch-darwin-c-opt
-export PETSC_OPTIONS="-options_file $BoussDev/rjl_examples/petscMPIoptions"
+export PETSC_OPTIONS="-options_file /Users/rjl/git/ImplicitAMR-paper/petscMPIoptions"
 export OMP_NUM_THREADS=6
 
 PS1='[\W] bouss $ '
